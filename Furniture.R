cab343a317fde3ade7f058a12828788916ff54fb
chapmcj <45241495+chapmcj@users.noreply.github.com>
2021-02-02 18:11:40 -0500

    Add files via upload

--- Furniture.R
+++ Furniture.R
@@ -3,11 +3,19 @@ install.packages("readxl")
 library(readxl)
  
 df_furniture <- read.csv("Furniture Sales Data.csv") %>%
-  select(-1:-2) 
+  select(-1:-2) %>%
+  mutate(Market = 'USCA')
+
+colnames(df_furniture)
 
 df_global <- read_excel("global_superstore_2016.xlsx") %>%
   rename_at(vars(everything()), ~str_replace_all(., "\\s+", ".")) %>%
-  select(-1)
+  #rename(Sub-Category = Sub.Category) %>%
+  select(-1, -20, -21, -22, -23, -24) 
 
-df_combined <- merge(df_furniture, df_global, by = "Order.ID", all = TRUE)
+names(df_global) <- gsub("-", ".", names(df_global))
+
+colnames(df_global)
+
+df_combined <- rbind(df_furniture, df_global)
 
